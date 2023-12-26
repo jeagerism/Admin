4bbe4422dd15a5215cbf49b8c1e952f26f173333
jeagerism <oo4dxuser@gmail.com>
2023-12-26 22:13:54 +0700

    remove

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,6 @@ import Visitor from './pages/Visitor'
 import House_Owner from './pages/House_Owner'
 import Search_bar from './pages/Search_bar'
 import Security from './pages/Security'
-import House_relation from './pages/House_relation'
 
 function App() {
   return (
@@ -22,8 +21,6 @@ function App() {
           <Route path='/visitor' element={<Visitor/>}/>
           <Route path='/houseOwner' element={<House_Owner/>}/>
           <Route path='/search' element={<Search_bar/>}/>
-          
-          <Route path='/house:id' element={<House_relation/>}/>
           
         </Routes>
       </BrowserRouter>
--- client/src/pages/House_relation.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function House_relation() {
-  return (
-    <div>House_relation</div>
-  )
-}
-
-export default House_relation
